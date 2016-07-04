10333f01d1a2db9b6e18c64df1001cadfcd65dc6
Daniel Koitka <daniel_koitka@outlook.com>
2016-07-05 08:05:50 +1000

    Added controller, currently does movement only

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed = 1.0f;
+    Rigidbody2D rb;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+    {
+        Vector2 movement;
+
+        GetInput(out movement);
+
+        Move(movement);
+	}
+
+    void Move(Vector2 vector)
+    {
+        rb.MovePosition(rb.position + vector * movementSpeed * Time.deltaTime);
+    }
+    void GetInput(out Vector2 output)
+    {
+        output = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+
+}
